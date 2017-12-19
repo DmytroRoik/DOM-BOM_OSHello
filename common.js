82fa021fce41082e625ee46d674046df58d86a2f
DmytroRoik <dmytroroik@gmail.com>
2017-12-19 16:33:21 +0200

    add hello to diferent OS

--- common.js
+++ common.js
@@ -1,17 +1,27 @@
 
-var navigatorMessageMap = {
-	'mac': 'macos',
-  'iphone': 'macos',
-  'win': 'windows'
-};
-
-var platform = navigator.platform.toLowerCase();
-console.log(platform);
-for (var key in navigatorMessageMap) {
-	key = key.toLowerCase();
-	var version = navigatorMessageMap[key];
-  
-  if (platform.indexOf(key) >= 0){
-		//
+var platform=navigator.platform.toLowerCase();
+if(/^mac/.test(platform)){
+	hello('Mac OS','http://www.gifmania.ca/Animated-Gifs-Technology/Animated-Images-Computing/Animations-Apple/Mac-Os/Mas-Os-X-Particles-89694.gif');
+}
+	else if(/^iphone/.test(platform)){
+		hello('IOS','https://research.shu.ac.uk/mmvl/viewfinder/macos.gif')
 	}
+	else if(/^win/.test(platform)){
+			hello('Windows','http://file.mobilmusic.ru/b6/b7/50/811527.gif')
+		 }
+	else if(/^linux/.test(platform)){
+		hello('linux','http://blee.ajpeterslearningsystems.com/Images/linux.gif');
+	}
+	else if(/^android/){
+		hello('Android','https://cdn-images-1.medium.com/max/1600/1*3x1QE3VzG5MWi5Qk6C-HLQ.gif');
+	}
+
+function hello(OS,url)
+{
+	var $h1=document.createElement('h1');
+		$h1.innerText="Hello from "+OS;
+	var $img=document.createElement('img');
+		$img.setAttribute('src',url);
+		 document.body.append($h1);
+		 document.body.append($img);
 }
